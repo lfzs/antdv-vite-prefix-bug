11a27935cf2198a6bc014296e91db759a0c98ce8
lfzs <zhanggong@jianzhikeji.com>
2023-11-01 15:12:43 +0800

    ConfigProvider prefixClas bug

---
+++ App.vue
@@ -0,0 +1,9 @@
+<template>
+<a-config-provider prefixCls="kkk">
+  <a-collapse>
+    <a-collapse-panel key="h1" header="This is panel header 1">1</a-collapse-panel>
+    <a-collapse-panel key="h2" header="This is panel header 2">2</a-collapse-panel>
+    <a-collapse-panel key="h3" header="This is panel header 3">3</a-collapse-panel>
+  </a-collapse>
+</a-config-provider>
+</template>
