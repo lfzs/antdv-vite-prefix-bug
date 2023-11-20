96d5e2a5c93398c9dd33628c131618e5b2c2b212
lfzs <jycomputer_417@163.com>
2023-11-20 10:34:34 +0800

    feat: menu animate bug demo

--- App.vue
+++ App.vue
@@ -1,9 +1,39 @@
 <template>
-<a-config-provider prefixCls="kkk">
-  <a-collapse>
-    <a-collapse-panel key="h1" header="This is panel header 1">1</a-collapse-panel>
-    <a-collapse-panel key="h2" header="This is panel header 2">2</a-collapse-panel>
-    <a-collapse-panel key="h3" header="This is panel header 3">3</a-collapse-panel>
-  </a-collapse>
-</a-config-provider>
+<div style="display: flex;">
+  <div>
+    <h2>修改 prefixCls 展开/折叠 动画无效</h2>
+    <a-config-provider prefixCls="kkk">
+      <a-menu style="width: 600px" mode="inline" :items="items" />
+    </a-config-provider>
+  </div>
+
+  <div>
+    <h2>未修改 prefixCls 展开/折叠 动画正常</h2>
+    <a-config-provider>
+      <a-menu style="width: 600px" mode="inline" :items="items" />
+    </a-config-provider>
+  </div>
+</div>
+
 </template>
+<script setup>
+const items = [
+  {
+    key: 'one',
+    label: 'Navigation One',
+    title: 'Navigation One',
+    children: [
+      {
+        key: 'option1',
+        label: 'Option 1',
+        title: 'Option 1',
+      },
+      {
+        key: 'option2',
+        label: 'Option 2',
+        title: 'Option 2',
+      }
+    ],
+  },
+]
+</script>
--- README.md
+++ README.md
@@ -1,21 +1,4 @@
 
 1、pnpm install && pnpm dev 启动项目
 
-2、访问 http://localhost:5173/
-
-3、collapse 展开/折叠动画消失
-
-4、移除 App.vue 文件中 a-config-provider 的 prefixCls 属性，动画正常
-
-```js
-// App.vue
-// 一旦指定 prefixCls 则 collapse 展开和折叠动画没了
-// 移除 prefixCls 则正常
-<a-config-provider prefixCls="kkk">
-  <a-collapse>
-    <a-collapse-panel key="h1" header="This is panel header 1">1</a-collapse-panel>
-    <a-collapse-panel key="h2" header="This is panel header 2">2</a-collapse-panel>
-    <a-collapse-panel key="h3" header="This is panel header 3">3</a-collapse-panel>
-  </a-collapse>
-</a-config-provider>
-```
+2、访问 http://localhost:5173 。点击 menu 组件
